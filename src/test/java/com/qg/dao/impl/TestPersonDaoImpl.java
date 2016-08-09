7c482d8c300d2161e03aa2b7461822ac42f5ec8e
QG2015 <fr479910770@qq.com>
2016-08-09 22:54:41 +0800

    fangrui

--- src/test/java/com/qg/dao/impl/TestPersonDaoImpl.java
+++
@@ -1,7 +0,0 @@
-package java.com.qg.dao.impl;
-
-/**
- * Created by tisong on 8/9/16.
- */
-public class TestPersonDaoImpl {
-}
--- src/test/java/com/qg/util/TestTimeUtil.java
+++
@@ -1,7 +0,0 @@
-package java.com.qg.util;
-
-/**
- * Created by tisong on 8/9/16.
- */
-public class TestTimeUtil {
-}
