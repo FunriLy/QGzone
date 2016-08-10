0a02e4b770d6867f5e73a2ec8f9c2198f8d3417b
Logan <153212028@qq.com>
2016-08-10 22:30:28 +0800

    longhui

--- src/main/java/com/qg/servlet/TwitterAdd.java
+++ src/main/java/com/qg/servlet/TwitterAdd.java
@@ -1,5 +1,128 @@
 package com.qg.servlet;
 
-public class TwitterAdd {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.servlet.ServletRequestContext;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+
+import com.google.gson.Gson;
+import com.qg.dao.impl.AlbumDaoImpl;
+import com.qg.model.TwitterModel;
+import com.qg.service.TwitterService;
+import com.qg.util.Logger;
+import com.qg.util.JsonUtil;
+import com.qg.util.Level;
+
+/**
+ * 
+ * @author dragon
+ * 
+ *         <pre>
+ *         上传说说的类
+ *         </pre>
+ */
+@WebServlet("/TwitterAdd")
+public class TwitterAdd extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(TwitterAdd.class);
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		int state;
+		int twitterPicture=0;
+		int twitterPictureId=0;
+		// 状态标志量
+		DataOutputStream output = new DataOutputStream(resp.getOutputStream());
+		Gson gson = new Gson();
+		TwitterModel twitter = null;
+		int talkId = userModel(request.getSession().getAttribute("user")).getuserId;
+		twitter.setTalkId(talkId);
+		try {
+			// 构造工厂
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			//设置文件阀值大小
+			factory.setSizeThreshold(10*1024);
+			//设置缓存文件区
+			String tempPath=getServletContext().getRealPath("/WEB-INF/twitter/tmp/");
+			factory.setRepository(new File(tempPath));
+			// 获得解析器
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			// 解决上传文件名 乱码问题
+			upload.setHeaderEncoding("utf-8");
+			// 对请求内容进行解析
+			List<FileItem> list = upload.parseRequest(new ServletRequestContext(request));
+			// 获得图片的张数
+			for (FileItem fileItem : list) {
+				if (!fileItem.isFormField()) {
+					twitterPicture++;
+				}
+			}
+			twitter.setTwitterPicture(twitterPicture);
+			//存进数据库且获得其说说id、
+			int twitterId=new TwitterService().addTwitter(twitter);
+			// 遍历集合
+			for (FileItem fileItem : list) {
+				if (fileItem.isFormField()) {
+					String name = fileItem.getFieldName();
+					String value = fileItem.getString("utf-8");
+					LOGGER.log(Level.INFO, "说说文字", name,value);
+					twitter.setTwitterWord(value);
+				} else {
+					// 定义限制上传的文件类型的字符串数组
+					String[] suffixs = new String[] {".jpg",".png","bmp","jpeg","jpeg2000","tiff","psd","swf"};
+					// 创建文件后缀过滤器
+					SuffixFileFilter filter = new SuffixFileFilter(suffixs);
+					//文件是否符合格式
+					if(filter.accept((File) fileItem))
+					{
+						twitterPictureId++;
+						InputStream in = new BufferedInputStream(fileItem.getInputStream());
+						// 获取路径
+						String path = getServletContext().getRealPath("/WEB-INF/twitter/");
+						System.out.println(path);
+						// 将文件写在服务器
+						OutputStream out = new BufferedOutputStream(new FileOutputStream(path+twitterId+"_"+twitterPictureId+".jpg"));
+						
+						int len=-1;
+						byte[] b = new byte[1024];
+						while ((len = in.read(b)) != -1) {
+							out.write(b,0,len);
+						}
+						out.close();
+						in.close();
+						state=201;
+					}else{
+						state=203;
+					}
+				}
+			}
+		} catch (Exception e) {
+			state=202;
+			LOGGER.log(Level.ERROR, "发表说说异常", e);
+		} finally {
+			JsonUtil<String, String> object = new JsonUtil(state);
+			output.write(gson.toJson(object).getBytes("UTF-8"));  
+			output.close();
+		}
+
+	}
 }
--- src/main/java/com/qg/servlet/TwitterDelete.java
+++ src/main/java/com/qg/servlet/TwitterDelete.java
@@ -1,5 +1,58 @@
 package com.qg.servlet;
 
-public class TwitterDelete {
+import java.io.DataOutputStream;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.qg.service.TwitterService;
+import com.qg.util.JsonUtil;
+import com.qg.util.Level;
+import com.qg.util.Logger;
+
+@WebServlet("/TwitterDelete")
+public class TwitterDelete extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = Logger.getLogger(TwitterDelete.class);
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		int state = 201;
+		try {
+			//获取说说id
+			int twitterId = Integer.getInteger(request.getParameter("twitterId"));
+			
+			TwitterService twitterService = new TwitterService();
+			// 获取路径
+			String path = getServletContext().getRealPath("/WEB-INF/twitter/");
+			//获取图片张数
+			int picture = twitterService.twitterPicture(twitterId);
+			//循环删除服务器上的图片
+			while(picture>0){
+				if(!twitterService.deleteFile(path+twitterId+"_"+picture+".jpg"))
+				{
+					state = 202;
+					break;
+				}
+				picture--;
+			}
+			//删除服务器上的说说信息
+			if(!twitterService.deleteTwitter(twitterId))
+			{
+				state = 202;
+			}
+		} catch (Exception e) {
+			LOGGER.log(Level.ERROR, "删除说说失败", e);
+			state =202;
+		}finally {
+				Gson gson = new Gson();
+				JsonUtil<String, String> jsonObject = new JsonUtil(state);
+				DataOutputStream output = new DataOutputStream(resp.getOutputStream());
+				output.write(gson.toJson(jsonObject).getBytes("UTF-8"));
+				output.close();
+		}
+	}
 }
